cb1c9d25b5530b72126d7d11ea347fa52c65e72a
otolithic <otolithic@users.noreply.github.com>
2018-10-15 23:59:46 -0400

    Removed styling and unnecessary jQuery
    Preparation for making this page more mobile-friendly. And also because after learning so much about canvas elements while working on monstermash I realized I was seriously underutilizing them here.

--- gamescripts.js
+++ gamescripts.js
@@ -1,64 +1,67 @@
-	var randocolor1 =[Math.floor(Math.random()*256), Math.floor(Math.random()*256), Math.floor(Math.random()*256)];
-	var randocolor2 =[Math.floor(Math.random()*256), Math.floor(Math.random()*256), Math.floor(Math.random()*256)];		
-	var middlecolor = [0,0,0];
+var randocolor1 =[Math.floor(Math.random()*256), Math.floor(Math.random()*256), Math.floor(Math.random()*256)];
+var randocolor2 =[Math.floor(Math.random()*256), Math.floor(Math.random()*256), Math.floor(Math.random()*256)];		
+var middlecolor = [0,0,0];
+var MIN_DIFFERENCE = 20;
+		
+//prevent too similar colors		
+for (x = 0; x < randocolor1.length; x++) {
+    while (Math.abs(randocolor1[x] - randocolor2[x]) < MIN_DIFFERENCE)
+        {randocolor1[x] = Math.floor(Math.random()*256);}		
+}
 
-$(document).ready(function(){
-	//generate rgb values	
+for (x = 0; x < middlecolor.length; x++) {
+    co = (randocolor1[x]-randocolor2[x])/2 + randocolor2[x];
+    co = Math.abs(Math.floor(co));
+    middlecolor[x] = co;	
+}
 
-	
-	var MIN_DIFFERENCE = 20;
-		
-	//prevent too similar colors		
-	for (x = 0; x < randocolor1.length; x++) {
-		while (Math.abs(randocolor1[x] - randocolor2[x]) < MIN_DIFFERENCE)
-			{randocolor1[x] = Math.floor(Math.random()*256);}		
-	}
-	
-	for (x = 0; x < middlecolor.length; x++) {
-		co = (randocolor1[x]-randocolor2[x])/2 + randocolor2[x];
-		co = Math.abs(Math.floor(co));
-		middlecolor[x] = co;	
-	}
-			
-	var hexcolor = ["#", "#", "#"];
-	//convert these rgb values to hex. [0] and [1] are the two colors and [2] is between them
-	for (x = 0; x < randocolor1.length; x++) {
-		var hex = [randocolor1[x].toString(16), randocolor2[x].toString(16), middlecolor[x].toString(16)];		
-		
-		for (y = 0; y < hex.length; y++) {
-			if (hex[y].length == 1) {
-				hex[y] = "0" + hex[y];	
-			}
-		}
-		
-		for (z = 0; z < hexcolor.length; z++) {
-			hexcolor[z] = hexcolor[z].concat(hex[z]);		
-		}
-	}
-		
-	
-	//populate the hex names and colors generated	
-	$("#colorname1").text(hexcolor[0]);
-	$("#colorname2").text(hexcolor[1]);	
-	$("#colorbox1").css("background-color", hexcolor[0]);
-	$("#colorbox2").css("background-color", hexcolor[1]);
-	
-	
-	//set sliders to 0
-	var form = document.getElementById("colorpicker");
-	form.elements["red"].value = 0;
-	form.elements["green"].value = 0;
-	form.elements["blue"].value = 0;
-	
-	//i'm putting this here cause I can't figure out how to integrate it with displayResults()
-	//but it scrolls down when the Done button is clicked
-	
-	$('.donebutton').click(function(){
-		$("html, body").animate({ scrollTop: 700}, 600);
-		return false;	
-	});
-})
+var hexcolor = ["#", "#", "#"];
+//convert these rgb values to hex. [0] and [1] are the two colors and [2] is between them
+for (x = 0; x < randocolor1.length; x++) {
+    var hex = [randocolor1[x].toString(16), randocolor2[x].toString(16), middlecolor[x].toString(16)];		
 
+    for (y = 0; y < hex.length; y++) {
+        if (hex[y].length == 1) {
+            hex[y] = "0" + hex[y];	
+        }
+    }
+
+    for (z = 0; z < hexcolor.length; z++) {
+        hexcolor[z] = hexcolor[z].concat(hex[z]);		
+    }
+}
+
+
+//populate the hex names and colors generated
+function draw() {
+  var canvas1 = document.getElementById("colorboxes");
+  var h = canvas1.height;
+  if (canvas1.getContext) {
+    var ctx = canvas1.getContext("2d"); 
+      var left = hexcolor[0];
+      var middle =  "#000000";
+      var right = hexcolor[1];
+      
+    ctx.fillStyle=left;
+    ctx.fillRect(0, 0, h, h);
+      
+    ctx.fillStyle=middle;
+    ctx.fillRect(h,0,h,h);
+      
+    ctx.fillStyle=right;
+    ctx.fillRect(2*h,0,h,h,0);
+      
+      ctx.fillStyle="white";
+      ctx.font = "150px serif";
+      ctx.fillText(left,h/2-ctx.measureText(left).width/2,h/2);
+      ctx.fillText(middle,1.5*h-ctx.measureText(middle).width/2,h/2);
+      ctx.fillText(right,2.5*h-ctx.measureText(right).width/2,h/2);
+      
+  }
+}
+
+//set sliders to 0
+	
 function getHexColor(r, g, b) {
 	var guesshex= "#";
 	var hex = 0;
@@ -86,24 +89,30 @@ function getRGB() {
 }
 
 function updateColor() {
-	var form = document.getElementById("colorpicker");
+    
+    var form = document.getElementById("colorpicker");
 	var redvalue = form.elements["redvalue"];
 	var greenvalue = form.elements["greenvalue"];
 	var bluevalue = form.elements["bluevalue"];
 	
-	var out = form.elements["middlecolorname"];
+	//var out = form.elements["middlecolorname"];
 	var rgb = getRGB();
-	
-		
 	
 	redvalue.value = rgb[0];
 	greenvalue.value = rgb[1];
 	bluevalue.value = rgb[2];	
 	hexcolor = getHexColor(rgb[0],rgb[1],rgb[2]);
 	
-	document.getElementById("colorboxmiddle").style.backgroundColor=hexcolor;	
-	
-	out.value = hexcolor;
+    var canvas1 = document.getElementById("colorboxes");
+    var h = canvas1.height;
+    if (canvas1.getContext) {
+        var ctx = canvas1.getContext("2d"); 
+        ctx.fillStyle=hexcolor;
+        ctx.fillRect(h,0,h,h);
+        
+        ctx.fillStyle="white";
+        ctx.fillText(hexcolor,1.5*h-ctx.measureText(hexcolor).width/2,h/2);
+    }
 }
 
 function displayResults() {
@@ -123,7 +132,7 @@ function displayResults() {
 	
 	// calculate and display the player's score based on their guess
 	scoreText.style.display = "block";
-	scoreText.innerHTML = "You guessed " + guess + ". Your score is: ";
+	scoreText.innerHTML = "You guessed " + guess + ".<br>The max score is 1000. Your score is: ";
 	score.style.display = "block";	
 	score.innerHTML = calcScore(middlecolor, rgb);
 }
@@ -134,6 +143,6 @@ function calcScore(actual, guess) {
 		sumSquares += Math.pow(actual[x]-guess[x], 2);	
 	}
 	
-	return Math.round(Math.sqrt(sumSquares));
+    score = Math.round(1000/Math.sqrt(sumSquares));
+	return score;
 }
-	
